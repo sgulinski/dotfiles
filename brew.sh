4bbbebc465493098fac49c1d37bf35b26a9d5517
Sebastian Gulinski <sebastian.gulinski@gmail.com>
2020-08-10 00:47:13 +0200

    Add missing Homebrew formulas

--- brew.sh
+++ brew.sh
@@ -13,6 +13,8 @@ BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+# See: https://ryanparman.com/posts/2019/using-gnu-command-line-tools-in-macos-instead-of-freebsd-tools/
+# See: https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
 brew install coreutils
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
@@ -37,6 +39,25 @@ brew install wget --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
+
+# Install other GNU non-core utilities
+brew install diffutils
+brew install ed
+brew install emacs
+brew install gawk
+# gdb requires further actions to make it work. See `brew info gdb`.
+brew install gdb
+brew install gnu-indent
+brew install gnu-tar
+brew install gnu-which
+brew install gnutls
+brew install gpatch
+brew install gzip
+brew install m4
+brew install make
+brew install nano
+brew install watch
+brew install wdiff
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
@@ -81,19 +102,28 @@ brew install xz
 # Install other useful binaries.
 brew install ack
 #brew install exiv2
+brew install dive
 brew install git
 brew install git-lfs
+brew install gradle
 brew install gs
 brew install imagemagick --with-webp
+brew install jenv
+brew install lastpass-cli
 brew install lua
 brew install lynx
+brew install maven
+brew install mc
+brew install packer
 brew install p7zip
 brew install pigz
+brew install pinentry-mac
 brew install pv
 brew install rename
 brew install rlwrap
 brew install ssh-copy-id
 brew install tree
+brew install unix2dos
 brew install vbindiff
 brew install zopfli
 
