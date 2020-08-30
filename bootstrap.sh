74029c2ca58e6ed6c6f60f51766b78e097be48b7
Sebastian Gulinski <sebastian.gulinski@gmail.com>
2020-08-31 01:22:57 +0200

    Add .extra coming from external git repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,8 @@ script_run_path=${PWD}
 script_path="$( cd $( dirname $0 ) && pwd )"
 script_name=$(basename $0)
 
-[ -f $script_path/.extra ] || { echo "Error: missing $script_path/.extra"; exit 1; }
-source $script_path/.extra
+[ -f $script_path/../extra/.extra ] || { echo "Error: missing $script_path/../extra/.extra"; exit 1; }
+source $script_path/../extra/.extra
 
 [ ! -z "$PROJECTS_DIR" ] || { echo "Error: \$PROJECTS_DIR not set in $script_path/.extra"; exit 1; }
 
@@ -22,13 +22,25 @@ fi
 $script_path/bin/ssh_add_private_keys
 
 # Install Homebrew formulas, Homebrew casks and Mac App Store applications
-$script_path/bin/ssh_add_private_keys
+$script_path/brew.sh
 
-# Clone git@github.com:sgulinski/dotfiles.git
+# Create directory for github repositories
 if [ ! -d $PROJECTS_DIR/github/sgulinski ]; then
   mkdir -p $PROJECTS_DIR/github/sgulinski
 fi
 
+# Clone git@github.com:sgulinski/extra.git
+cd $PROJECTS_DIR/github/sgulinski;
+if [ ! -d $PROJECTS_DIR/github/sgulinski/extra ]; then
+  echo 'Cloning extra repository'
+  git clone git@github.com:sgulinski/extra.git ./extra;
+else
+  echo 'extra repository already cloned - updating'
+  cd ./extra
+  git pull
+fi
+
+# Clone git@github.com:sgulinski/dotfiles.git
 cd $PROJECTS_DIR/github/sgulinski;
 if [ ! -d $PROJECTS_DIR/github/sgulinski/dotfiles ]; then
   echo 'Cloning dotfiles repository'
@@ -42,13 +54,15 @@ fi
 cd $script_run_path
 
 # Create symbolic links
-echo "Creating symbolink links pointing from $PROJECTS_DIR/github/sgulinski/dotfiles/.* to $HOME"	
+echo "Creating symbolink link pointing from $PROJECTS_DIR/github/sgulinski/extra/.extra to $HOME"
+ln -sfn $PROJECTS_DIR/github/sgulinski/extra/.extra $HOME/.extra
+
+echo "Creating symbolink links pointing from $PROJECTS_DIR/github/sgulinski/dotfiles/.* to $HOME"
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/bin $HOME/bin
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.aliases $HOME/.aliases
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.bash_profile $HOME/.bash_profile
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.bash_prompt  $HOME/.bash_prompt
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.bashrc $HOME/.bashrc
-ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.extra $HOME/.extra
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.gitconfig $HOME/.gitconfig
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.gitconfig $HOME/.gitconfig
 ln -sfn $PROJECTS_DIR/github/sgulinski/dotfiles/.gitignore $HOME/.gitignore
